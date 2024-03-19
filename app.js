e3f8f6803368ac360981dffd9b8a9eb86a95992c
iquping <iquping@gmail.com>
2024-03-19 15:39:40 -0500

    simulate component

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "./views")));
 app.set("view engine", "html");
 app.engine("html", ejs.renderFile);
 
